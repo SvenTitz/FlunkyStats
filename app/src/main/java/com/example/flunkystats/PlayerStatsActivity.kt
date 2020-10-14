f4b459bf27beac8372c370fd0c1f5e2927b72804
Sven0171 <sven.titz@rwth-aachen.de>
2020-10-14 14:49:46 +0200

    fix to team name progress bar

--- app/src/main/java/com/example/flunkystats/PlayerStatsActivity.kt
+++ app/src/main/java/com/example/flunkystats/PlayerStatsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.annotation.RequiresApi
@@ -23,13 +24,20 @@ class PlayerStatsActivity: AppCompatActivity(), LoadsData {
     private val teamMembRef = database.getReference("TeamMembership")
     private val teamsRef = database.getReference("Teams")
 
+    private lateinit var teamPgsBar: ProgressBar
+    private var countTeamsLoading = 0;
+    private var teamNames: ArrayList<String> = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_stats)
 
+        teamPgsBar = addProgressBar(findViewById<LinearLayout>(R.id.llPTeams), this)
+
         var playerID = intent.getStringExtra(AppConfig.EXTRA_MESSAGE_PLAYER_ID)
 
         if(playerID == null) {
+            //throw proper popup error
             //throw proper popup error
             Log.w("Sven", "player ID could not be transfered")
             return
@@ -41,26 +49,6 @@ class PlayerStatsActivity: AppCompatActivity(), LoadsData {
         loadPlayerName(playerID)
 
         loadPlayerTeams(playerID)
-
-//        tvPTeam1.setOnClickListener {
-//            val intent = Intent(this, TeamStatsActivity::class.java).apply {
-//                putExtra(AppConfig.EXTRA_MESSAGE_TEAM, tvPTeam1.text)
-//            }
-//            startActivity(intent)
-//        }
-//        tvPTeam2.setOnClickListener {
-//            val intent = Intent(this, TeamStatsActivity::class.java).apply {
-//                putExtra(AppConfig.EXTRA_MESSAGE_TEAM, tvPTeam2.text)
-//            }
-//            startActivity(intent)
-//        }
-//        tvPTeam3.setOnClickListener {
-//            val intent = Intent(this, TeamStatsActivity::class.java).apply {
-//                putExtra(AppConfig.EXTRA_MESSAGE_TEAM, tvPTeam3.text)
-//            }
-//            startActivity(intent)
-//        }
-
 
     }
 
@@ -85,8 +73,7 @@ class PlayerStatsActivity: AppCompatActivity(), LoadsData {
 
     private fun loadPlayerTeams(playerID: String) {
         //add progress bar
-        val pgsBar = addProgressBar(findViewById<LinearLayout>(R.id.llPTeams), this)
-        pgsBar.visibility = View.VISIBLE
+        teamPgsBar.visibility = View.VISIBLE
 
         //search for teammemberships of play with ID: $memberID
         val teamMembQ = teamMembRef.orderByChild("memberID").equalTo(playerID)
@@ -106,10 +93,9 @@ class PlayerStatsActivity: AppCompatActivity(), LoadsData {
                     if(teamID == null) {
                         return
                     }
-                    //read the team name and create new textview with the name
+                    //read the team name and add it to the teamNames array list
                     loadTeamName(teamID)
                 }
-                pgsBar.visibility = View.GONE
             }
             override fun onCancelled (error: DatabaseError) {
                 Log.w("Sven", "Failed to read value.", error.toException())
@@ -118,6 +104,8 @@ class PlayerStatsActivity: AppCompatActivity(), LoadsData {
     }
 
     private fun loadTeamName(teamID: String) {
+        countTeamsLoading++
+
         val teamQ = teamsRef.orderByKey().equalTo(teamID)
         teamQ.addListenerForSingleValueEvent(object : ValueEventListener {
 
@@ -132,12 +120,26 @@ class PlayerStatsActivity: AppCompatActivity(), LoadsData {
                     Log.w("Sven", "Team name is null")
                     return
                 }
-                createTeamTextView(teamName)
+                teamNames.add(teamName)
+                countTeamsLoading--
+                createTeamTextViews()
             }
             override fun onCancelled (error: DatabaseError) {
                 Log.w("Sven", "Failed to read value.", error.toException())
             }
         })
+    }
+
+    private fun createTeamTextViews() {
+        if(countTeamsLoading != 0) {
+            return
+        }
+        else {
+            teamPgsBar.visibility = View.GONE
+            teamNames.forEach { name ->
+                createTeamTextView(name)
+            }
+        }
     }
 
     private fun createTeamTextView(teamName: String): TextView {
